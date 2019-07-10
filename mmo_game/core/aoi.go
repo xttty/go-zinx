27234f3efdefb0078927f58de24cbb78ebd0e5cd
熊添翼 <xty@meitu.com>
2019-07-10 15:02:40 +0800

    mmo_game 项目代码目录初始化、消息初始化

--- mmo_game/core/aoi.go
+++ mmo_game/core/aoi.go
@@ -1,6 +1,9 @@
 package core
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // AOIManager AOI管理实例
 type AOIManager struct {
@@ -93,4 +96,88 @@ func (aoi *AOIManager) GetSurroundGridByGID(gID int, level int) []*Grid {
 		}
 	}
 	return surroundGrids
+}
+
+// GetGIDByPos 根据坐标得到格子ID
+func (aoi *AOIManager) GetGIDByPos(x, y float32) (int, error) {
+	if x < float32(aoi.MinX) || x > float32(aoi.MaxX) {
+		return 0, errors.New("out of range")
+	}
+	if y < float32(aoi.MinY) || y > float32(aoi.MaxY) {
+		return 0, errors.New("out of range")
+	}
+	gx := (int(x) - aoi.MinX) / aoi.gridWidth()
+	gy := (int(y) - aoi.MinY) / aoi.gridHeight()
+	gID := gy*aoi.CntX + gx
+	return gID, nil
+}
+
+// GetSurroundPlayerIDs 根据格子ID获得周围所有player
+func (aoi *AOIManager) GetSurroundPlayerIDs(gID, level int) ([]int, error) {
+	baseGrid, exist := aoi.grids[gID]
+	if !exist {
+		return nil, errors.New("grid is not exist")
+	}
+	var playerIDs []int
+	surroundGrids := aoi.GetSurroundGridByGID(gID, level)
+	playerIDs = append(playerIDs, baseGrid.GetPlayerIDs()...)
+	for _, grid := range surroundGrids {
+		playerIDs = append(playerIDs, grid.GetPlayerIDs()...)
+	}
+	return playerIDs, nil
+}
+
+// GetSurroundPlayerIDsByPos 根据坐标获得周围所有player的ID
+func (aoi *AOIManager) GetSurroundPlayerIDsByPos(x, y float32, level int) ([]int, error) {
+	baseGID, err := aoi.GetGIDByPos(x, y)
+	if err != nil {
+		return nil, err
+	}
+	playerIDs, err := aoi.GetSurroundPlayerIDs(baseGID, level)
+	return playerIDs, err
+}
+
+// GetPlayerIDsByGID 获取特定格子中的所有playerID
+func (aoi *AOIManager) GetPlayerIDsByGID(gID int) []int {
+	grid, exist := aoi.grids[gID]
+	if !exist {
+		return nil
+	}
+	return grid.GetPlayerIDs()
+}
+
+// RemovePlayerFromGrid 将player从一个格子中移除
+func (aoi *AOIManager) RemovePlayerFromGrid(pID, gID int) {
+	grid, exist := aoi.grids[gID]
+	if !exist {
+		return
+	}
+	grid.Remove(pID)
+}
+
+// AddPlayerToGrid 将player添加到格子里
+func (aoi *AOIManager) AddPlayerToGrid(pID, gID int) {
+	grid, exist := aoi.grids[gID]
+	if !exist {
+		return
+	}
+	grid.Add(pID)
+}
+
+// AddPlayerToGridByPos 将player通过坐标添加到相应的格子中
+func (aoi *AOIManager) AddPlayerToGridByPos(pID int, x, y float32) {
+	gID, err := aoi.GetGIDByPos(x, y)
+	if err != nil {
+		return
+	}
+	aoi.AddPlayerToGrid(pID, gID)
+}
+
+// RemovePlayerFromGridByPos 将player通过坐标从相应格子中移除
+func (aoi *AOIManager) RemovePlayerFromGridByPos(pID int, x, y float32) {
+	gID, err := aoi.GetGIDByPos(x, y)
+	if err != nil {
+		return
+	}
+	aoi.RemovePlayerFromGrid(pID, gID)
 }
